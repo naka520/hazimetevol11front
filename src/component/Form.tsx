ef978da4dd4777705739b89977d39616b9de749a
naka520 <yashipipi0520@gmail.com>
2023-11-04 16:14:26 +0900

    [modify]FormにAPI接続の確認用のソースを追加

--- src/component/Form.tsx
+++ src/component/Form.tsx
@@ -1,8 +1,12 @@
 import React, { useState } from "react";
+import { useEffect } from "react";
+interface HelloResponse {
+  message: string;
+}
 
 const IncomeRegistration: React.FC = () => {
   const [entries, setEntries] = useState([0]);
-
+  const [apiResponse, setApiResponse] = useState<HelloResponse | null>(null);
   const addEntry = () => {
     setEntries(prev => [...prev, prev.length]);
   };
@@ -11,11 +15,38 @@ const IncomeRegistration: React.FC = () => {
     setEntries(prev => prev.filter((_, index) => index !== indexToRemove));
   };
 
+  useEffect(() => {
+    // ローカルのFastAPIサーバーに対してGETリクエストを行う
+    fetch("http://localhost:8000/hello")
+      .then(response => {
+        if (!response.ok) {
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        setApiResponse(data); // Save the data in state
+      })
+      .catch(error => console.error("Error fetching data: ", error));
+
+    if (apiResponse) {
+      // It's safe to access properties of apiResponse here
+      console.log(apiResponse.message);
+    } else {
+      // Handle the case where apiResponse is null (e.g., display a loading indicator)
+      console.log("apiResponse is not available yet.");
+    }
+  }, []); // Empty dependency array means this effect will only run once, similar to componentDidMount
+
   return (
     <div className="min-h-screen bg-yellow-50 flex items-center justify-center">
       <div className="bg-white p-8 rounded-md shadow-md w-5/6 md:w-3/4 lg:w-2/3">
         <div className="bg-blue-400 text-white rounded-full p-3 mb-6 w-3/4 mx-auto text-center shadow-md">
           今月稼ぐ予定の収入を教えてください！
+        </div>
+        <div className="bg-blue-400 text-white rounded-full p-3 mb-6 w-3/4 mx-auto text-center shadow-md">
+          {apiResponse ? apiResponse.message : "Loading..."}
         </div>
         <div className="mb-8 flex justify-between items-center">
           {["カテゴリ", "時間", "時給", "日にち"].map((header, index) => (
