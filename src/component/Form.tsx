de8549bd8f6b7a4b5b8b6d7cdecf5c07244fef56
naka520 <yashipipi0520@gmail.com>
2023-11-05 12:55:20 +0900

    [add]収入計算のロジック追加

--- src/component/Form.tsx
+++ src/component/Form.tsx
@@ -1,53 +1,66 @@
 import React, { useState } from "react";
-import { useEffect } from "react";
-interface HelloResponse {
-  message: string;
+
+interface Entry {
+  category: string;
+  hours: number;
+  hourlyRate: number;
+  workDays: number;
 }
 
 const IncomeRegistration: React.FC = () => {
-  const [entries, setEntries] = useState([0]);
-  const [apiResponse, setApiResponse] = useState<HelloResponse | null>(null);
+  const [entries, setEntries] = useState<Entry[]>([
+    { category: "", hours: 0, hourlyRate: 0, workDays: 0 },
+  ]);
+
   const addEntry = () => {
-    setEntries(prev => [...prev, prev.length]);
+    setEntries([
+      ...entries,
+      { category: "", hours: 0, hourlyRate: 0, workDays: 0 },
+    ]);
   };
 
   const removeEntry = (indexToRemove: number) => {
-    setEntries(prev => prev.filter((_, index) => index !== indexToRemove));
+    setEntries(entries.filter((_, index) => index !== indexToRemove));
   };
 
-  useEffect(() => {
-    // ローカルのFastAPIサーバーに対してGETリクエストを行う
-    fetch("http://localhost:8000/hello")
-      .then(response => {
-        if (!response.ok) {
-          throw new Error("Network response was not ok");
-        }
-        return response.json();
-      })
-      .then(data => {
-        console.log(data);
-        setApiResponse(data); // Save the data in state
-      })
-      .catch(error => console.error("Error fetching data: ", error));
-
-    if (apiResponse) {
-      // It's safe to access properties of apiResponse here
-      console.log(apiResponse.message);
+  const handleInputChange = (
+    index: number,
+    field: keyof Entry,
+    value: string
+  ) => {
+    const newEntries = [...entries];
+    // カテゴリーの場合はそのままstringを設定し、それ以外の数値フィールドにはNumberで変換した値を設定
+    if (field === "category") {
+      (newEntries[index][field] as string) = value;
     } else {
-      // Handle the case where apiResponse is null (e.g., display a loading indicator)
-      console.log("apiResponse is not available yet.");
+      (newEntries[index][field] as number) = Number(value);
     }
-  }, []); // Empty dependency array means this effect will only run once, similar to componentDidMount
+    setEntries(newEntries);
+  };
+  const calculateTotalIncome = (): number => {
+    return entries.reduce((acc, entry) => {
+      return acc + entry.hours * entry.hourlyRate * entry.workDays;
+    }, 0);
+  };
+
+  const getIncomeStatusMessage = (): string => {
+    const totalIncome = calculateTotalIncome();
+    if (totalIncome >= 108000) {
+      return "合計金額10万8千円以上で社会保険の扶養を超えます。";
+    } else if (totalIncome >= 85000) {
+      return "合計金額8万5千円以上で所得税非課税の月割り範囲外です。";
+    }
+    return "非課税（月割り）かつ社会保険の扶養範囲内です。";
+  };
+
+  const handleSubmit = () => {
+    // APIへのPOSTリクエストのロジックを実装する
+  };
 
   return (
     <div className="min-h-screen bg-yellow-50 flex items-center justify-center">
       <div className="bg-white p-8 rounded-md shadow-md w-5/6 md:w-3/4 lg:w-2/3">
-        <div className="bg-blue-400 text-white rounded-full p-3 mb-6 w-3/4 mx-auto text-center shadow-md">
-          今月稼ぐ予定の収入を教えてください！
-        </div>
-        <div className="bg-blue-400 text-white rounded-full p-3 mb-6 w-3/4 mx-auto text-center shadow-md">
-          {apiResponse ? apiResponse.message : "Loading..."}
-        </div>
+        {/* ... */}
         <div className="mb-8 flex justify-between items-center">
           {["カテゴリ", "時間", "時給", "日にち"].map((header, index) => (
             <span key={index} className="flex-1 text-center">
@@ -56,34 +69,64 @@ const IncomeRegistration: React.FC = () => {
           ))}
           <span className="w-8 h-8"></span>
         </div>
-
-        {entries.map((_, index) => (
+        {entries.map((entry, index) => (
           <div key={index} className="mb-2 flex justify-between items-center">
-            {new Array(4).fill(0).map((_, idx) => (
-              <input
-                key={idx}
-                type="text"
-                className="flex-1 p-2 border rounded-md text-center mx-1  bg-gray-100"
-                placeholder="..."
-              />
-            ))}
+            <input
+              type="text"
+              className="flex-1 p-2 border rounded-md text-center mx-1 bg-gray-100"
+              placeholder="カテゴリ"
+              value={entry.category}
+              onChange={e =>
+                handleInputChange(index, "category", e.target.value)
+              }
+            />
+            <input
+              type="number"
+              className="flex-1 p-2 border rounded-md text-center mx-1 bg-gray-100"
+              placeholder="時間"
+              value={entry.hours}
+              onChange={e => handleInputChange(index, "hours", e.target.value)}
+            />
+            <input
+              type="number"
+              className="flex-1 p-2 border rounded-md text-center mx-1 bg-gray-100"
+              placeholder="時給"
+              value={entry.hourlyRate}
+              onChange={e =>
+                handleInputChange(index, "hourlyRate", e.target.value)
+              }
+            />
+            <input
+              type="number"
+              className="flex-1 p-2 border rounded-md text-center mx-1 bg-gray-100"
+              placeholder="日数"
+              value={entry.workDays}
+              onChange={e =>
+                handleInputChange(index, "workDays", e.target.value)
+              }
+            />
             <button
-              className="ml-2 w-8 h-8 bg-gray-200 rounded-full flex items-center justify-center"
+              className="ml-2 w-8 h-8 bg-red-200 rounded-full flex items-center justify-center"
               onClick={() => removeEntry(index)}
             >
               -
             </button>
           </div>
         ))}
-
+        {/* ... */}
         <div className="mt-6 text-center">
+          <p className="text-lg font-bold mb-4">{`合計金額: ¥${calculateTotalIncome().toLocaleString()}`}</p>
+          <p className="text-md mb-4">{getIncomeStatusMessage()}</p>
           <button
-            className="bg-blue-300 hover:bg-gray-300 text-white rounded-md px-4 py-2 shadow-md mr-4"
+            className="bg-blue-300 hover:bg-blue-200 text-white rounded-md px-4 py-2 shadow-md mr-4"
             onClick={addEntry}
           >
             フォームを追加
           </button>
-          <button className="bg-blue-500 hover:bg-gray-300 text-white rounded-md px-4 py-2 shadow-md">
+          <button
+            className="bg-blue-500 hover:bg-blue-400 text-white rounded-md px-4 py-2 shadow-md"
+            onClick={handleSubmit}
+          >
             登録
           </button>
         </div>
--- src/component/SignIn.tsx
+++ src/component/SignIn.tsx
@@ -1,30 +1,61 @@
 // SignIn.tsx
 import React from "react";
+import { useState } from "react";
+import axios from "axios";
 
 const SignIn: React.FC = () => {
+  const [username, setUsername] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleLogin = async () => {
+    try {
+      const response = await axios.post("http://localhost:8000/login", {
+        username,
+        email,
+        password,
+      });
+
+      // ここでトークンを保存するなどの処理を行います
+      // 例: localStorage.setItem('token', response.data.token);
+      console.log("Login successful", response);
+    } catch (error) {
+      // エラーハンドリング
+      console.error("Login failed", error);
+    }
+  };
   return (
     <div className="min-h-screen bg-yellow-50 flex items-center justify-center">
       <div className="bg-white p-12 rounded-md shadow-md w-4/5 md:w-2/3 lg:w-1/2">
-        <div className="mb-8">
-          <input
-            className="bg-gray-100 p-3 rounded-md w-full mb-4 placeholder-gray-500"
-            placeholder="username"
-          />
-          <input
-            className="bg-gray-100 p-3 rounded-md w-full mb-4 placeholder-gray-500"
-            placeholder="mail"
-          />
-          <input
-            className="bg-gray-100 p-3 rounded-md w-full mb-4 placeholder-gray-500"
-            type="password"
-            placeholder="password"
-          />
-        </div>
-        <div className="text-center">
-          <button className="bg-blue-300 hover:bg-gray-400 text-white p-3 rounded-md w-full">
-            ログイン
-          </button>
-        </div>
+        <form
+          onSubmit={e => {
+            e.preventDefault();
+            handleLogin();
+          }}
+        >
+          <div className="mb-8">
+            <input
+              className="bg-gray-100 p-3 rounded-md w-full mb-4 placeholder-gray-500"
+              placeholder="username"
+              onChange={e => setUsername(e.target.value)}
+            />
+            <input
+              className="bg-gray-100 p-3 rounded-md w-full mb-4 placeholder-gray-500"
+              placeholder="mail"
+              onChange={e => setEmail(e.target.value)}
+            />
+            <input
+              className="bg-gray-100 p-3 rounded-md w-full mb-4 placeholder-gray-500"
+              type="password"
+              placeholder="password"
+              onChange={e => setPassword(e.target.value)}
+            />
+          </div>
+          <div className="text-center">
+            <button className="bg-blue-300 hover:bg-gray-400 text-white p-3 rounded-md w-full">
+              ログイン
+            </button>
+          </div>
+        </form>
       </div>
     </div>
   );
